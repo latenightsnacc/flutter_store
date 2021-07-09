dc7389c5741107a61b055eb490208df3bf38e729
Deborah <54311514+latenightsnacc@users.noreply.github.com>
2021-07-09 06:46:28 +0100

    added a new widget to store product_control

---
+++ lib/product_control.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProductControl extends StatelessWidget {
+  final Function addProduct;
+
+  ProductControl(this.addProduct);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        addProduct('Munchies');
+      },
+      child: Text('Add Product'),
+    );
+  }
+}
